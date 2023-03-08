324ef609bb16571086da9594ca91c182457fd7d7
david <dalogar0801@g.educaand.es>
2023-03-08 22:04:15 +0100

    año

--- Herencia12/src/herencia12/Libro.java
+++ Herencia12/src/herencia12/Libro.java
@@ -4,13 +4,13 @@ public class Libro extends Publicacion {
 
     private boolean prestado;
 
-    public Libro(String isbn, String titulo, int anio) {
-        super(isbn, titulo, anio);
+    public Libro(String isbn, String titulo, int año) {
+        super(isbn, titulo, año);
         this.prestado = false;
     }
 
     public void presta() {
-        if (this.estaPrestado()) {
+        if (this.prestado) {
             System.out.println("Lo siento, ese libro ya está prestado.");
         } else {
             this.prestado = true;
